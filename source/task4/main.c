1ca4f3f74434a8aa6cf65c754a1a248bf6a21da4
Alex Manak <alexmanak@icloud.com>
2023-04-27 01:52:55 -0700

    greyscaling complete

--- source/task4/main.c
+++ source/task4/main.c
@@ -2,91 +2,53 @@
 #include "vga_plot.c"
 volatile unsigned *vga = (volatile unsigned *) 0x00004000; /* VGA adapter base address */
 #define STRING_LOCATION (char *) 0x00008000
-
 unsigned char pixel_list[] = {
 	#include "../../data/misc/pixels.txt"
 };
 unsigned num_pixels = sizeof(pixel_list)/2;
-
 int main() {
-	unsigned y, x, row, column, colour, i, j, pixel = 0;
-	sprintf(STRING_LOCATION, "Hello World!");
+	unsigned y, x, colour, pixel;
 	for (y = 0; y < 120; y++) {
 		for (x =0; x < 160; x++) {
 			vga_plot(x, y, 0);
 		}
 	}
-	/*
-		CONVOLUTION for greyscaling:
-		Unfortunately, the pixel_list[] array doesn't list the pixel coordiante pairs in order, so for each pixel
-		we must search through the WHOLE list for the adjacent pixels. 
-	*/
-	for (pixel=0; pixel<num_pixels; pixel++) {
-		// get coordinates for first pixel from first pair in the pixel_list[] array
-		x = pixel_list[2*pixel];
-		y = pixel_list[2*pixel+1];
-		// go through entire list of pixels...
-		for (i=0; i<num_pixels; i++) {
-			// check if pixel is two rows above or below
-			if ((pixel_list[2*i] == x - 2) || (pixel_list[2*i] == x + 2)) {
-				// check if it is within (-2) to (+2) columns of the main pixel
-				if (pixel_list[2*i+1] == y - 2) {
-					colour += 1;
-				} else if (pixel_list[2*i+1] == y - 1) {
-					colour += 2;
-				} else if (pixel_list[2*i+1] == y) {
-					colour += 4;
-				} else if (pixel_list[2*i+1] == y + 1) {
-					colour += 2;
-				} else if (pixel_list[2*i+1] == y + 2) {
-					colour += 1;
+	for (y=0; y<120; y++) {
+		for (x=0; x<160; x++) {
+			// search through entire pixel_list[] for pixels that are within desired vicinity, and add weights accordingly
+			colour = 0;
+			pixel = 0;
+			while (pixel <= num_pixels) {
+				if ((pixel_list[2*pixel] == x - 2) || (pixel_list[2*pixel] == x + 2)) {
+					if ((pixel_list[2*pixel+1] == y - 2) || (pixel_list[2*pixel+1] == y + 2)) {
+						colour += 1;
+					} else if ((pixel_list[2*pixel+1] == y - 1) || (pixel_list[2*pixel+1] == y + 1)) {
+						colour += 2;
+					} else if (pixel_list[2*pixel+1] == y) {
+						colour += 4;
+					}
+				} else if ((pixel_list[2*pixel] == x - 1) || (pixel_list[2*pixel] == x + 1)) {
+					if ((pixel_list[2*pixel+1] == y - 2) || (pixel_list[2*pixel+1] == y + 2)) {
+						colour += 2;
+					} else if ((pixel_list[2*pixel+1] == y - 1) || (pixel_list[2*pixel+1] == y + 1)) {
+						colour += 4;
+					} else if (pixel_list[2*pixel+1] == y) {
+						colour += 8;
+					}
+				} else if (pixel_list[2*pixel] == x) {
+					if ((pixel_list[2*pixel+1] == y - 2) || (pixel_list[2*pixel+1] == y + 2)) {
+						colour += 4;
+					} else if ((pixel_list[2*pixel+1] == y - 1) || (pixel_list[2*pixel+1] == y + 1)) {
+						colour += 8;
+					} else if (pixel_list[2*pixel+1] == y) {
+						colour += 16;
+					}
 				}
+				pixel++;
 			}
-			// check if pixel is one row above or below
-			if ((pixel_list[2*i] == x - 1) || (pixel_list[2*i] == x + 1)) {
-				// check if it is within (-2) to (+2) columns of the main pixel
-				if (pixel_list[2*i+1] == y - 2) {
-					colour += 2;
-				} else if (pixel_list[2*i+1] == y - 1) {
-					colour += 4;
-				} else if (pixel_list[2*i+1] == y) {
-					colour += 8;
-				} else if (pixel_list[2*i+1] == y + 1) {
-					colour += 4;
-				} else if (pixel_list[2*i+1] == y + 2) {
-					colour += 2;
-				}
-			}
-			// check if pixel is in same row
-			if (pixel_list[2*i] == x) {
-				// check if it is within (-2) to (+2) columns of the main pixel
-				if (pixel_list[2*i+1] == y - 2) {
-					colour += 4;
-				} else if (pixel_list[2*i+1] == y - 1) {
-					colour += 8;
-				} else if (pixel_list[2*i+1] == y) {
-					colour += 16;
-				} else if (pixel_list[2*i+1] == y + 1) {
-					colour += 8;
-				} else if (pixel_list[2*i+1] == y + 2) {
-					colour += 4;
-				}
-			}
+			vga_plot(x, y, (255*colour)/100);			
 		}
-		vga_plot(x, y, (255*colour)/100);
-		pixel++;
 	}
 }
 
-/*
-	ORIGINAL BLACK/WHITE PRINTING ALGORITHM
-
-while (pixel <= num_pixels) {
-		x = pixel_list[2*pixel];
-		y = pixel_list[2*pixel+1];
-		vga_plot(x, y, 255);
-		pixel++;
-	}
-
-
-*/
+// WRITE YOUR OWN VGA ADAPTER SO YOU CAN INCREASE THE RESOLUTION 
