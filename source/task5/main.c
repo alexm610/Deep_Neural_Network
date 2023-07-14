823f0459f0760e80459570d0852d7c53650ab601
Alex Manak <alexmanak@icloud.com>
2023-07-14 14:02:33 -0700

    faster algo. attempt failure...

--- source/task5/main.c
+++ source/task5/main.c
@@ -6,84 +6,59 @@ volatile unsigned *wordcopy = (volatile unsigned *) 0x00001040; /* Wordcopy modu
 unsigned char pixel_list[] = {
 	#include "../../data/misc/pixels.txt"
 };
+unsigned char pixel_array[120][160];
+unsigned char blurred_image[120][160];
+unsigned char weights[5][5] = {	{1, 2, 4, 2, 1},
+								{2, 4, 8, 4, 2,},
+								{4, 8, 16, 8, 4},
+								{2, 4, 8, 4, 2},
+								{1, 2, 4, 2, 1}
+};
 unsigned num_pixels = sizeof(pixel_list)/2;
 int main() {
-	unsigned i, y, x, colour, pixel;
-	
+	unsigned i, j, y, x, colour, pixel;
+/*	
 	// VGA Blurred dog printing 
 	for (y = 0; y < 120; y++) {
 		for (x =0; x < 160; x++) {
 			vga_plot(x, y, 0);
+			pixel_array[y][x] = 0;
+			blurred_image[y][x] = 0;
+		}
+	}
+
+	pixel = 0;
+	while (pixel <= num_pixels) {
+		x = pixel_list[2*pixel];
+		y = pixel_list[2*pixel+1];
+
+		pixel_array[y][x] = 1;
+		pixel++;
+	}
+	/*
+	pixel = 0;
+	for (x = 0; x < 160; x++) {
+		for (y = 0; y < 120; y++) {
+			for (i = 0; i < 5; i++) {
+				for (j = 0; j < 5; j++) {
+					blurred_image[y][x] += weights[i][j] * pixel_array[y+i-3][x+j-3];
+				}
+			}
+		}
+	}
+
+	for (y = 0; y < 120; y++) {
+		for (x =0; x < 160; x++) {
+			vga_plot(x, y, (255*pixel_array[y][x]));
 		}
 	}
 
 
-	for (y=0; y<120; y++) {
-		for (x=0; x<160; x++) {
-			// search for the first pixel that is TWO COLUMNS LEFT of the current coordinate
-			colour = 0;
-			pixel = 0;
-			while (pixel <= num_pixels) {
-				if (pixel_list[2*pixel+1] == y - 2) {
-					break;
-				}
-				pixel++;
-			}
-			// we have found the portion of the list that is TWO COLUMNS LEFT OF THE CURRENT COORDINATE
-			while (pixel_list[2*pixel+1] == y - 2) {
-				if ((pixel_list[2*pixel] == x - 2) || (pixel_list[2*pixel] == x - 2)) {
-					colour += 1;
-				} else if ((pixel_list[2*pixel] == x - 1) || (pixel_list[2*pixel] == x - 1)) {
-					colour += 2;
-				} else if (pixel_list[2*pixel] == x) {
-					colour += 4;
-				}
-				pixel++;
-			}
-			while (pixel_list[2*pixel+1] == y - 1) {
-				if ((pixel_list[2*pixel] == x - 2) || (pixel_list[2*pixel] == x - 2)) {
-					colour += 2;
-				} else if ((pixel_list[2*pixel] == x - 1) || (pixel_list[2*pixel] == x - 1)) {
-					colour += 4;
-				} else if (pixel_list[2*pixel] == x) {
-					colour += 8;
-				}
-				pixel++;
-			}
-			while (pixel_list[2*pixel+1] == y) {
-				if ((pixel_list[2*pixel] == x - 2) || (pixel_list[2*pixel] == x - 2)) {
-					colour += 4;
-				} else if ((pixel_list[2*pixel] == x - 1) || (pixel_list[2*pixel] == x - 1)) {
-					colour += 8;
-				} else if (pixel_list[2*pixel] == x) {
-					colour += 16;
-				}
-				pixel++;
-			}
-			while (pixel_list[2*pixel+1] == y + 1) {
-				if ((pixel_list[2*pixel] == x - 2) || (pixel_list[2*pixel] == x - 2)) {
-					colour += 2;
-				} else if ((pixel_list[2*pixel] == x - 1) || (pixel_list[2*pixel] == x - 1)) {
-					colour += 4;
-				} else if (pixel_list[2*pixel] == x) {
-					colour += 8;
-				}
-				pixel++;
-			}
-			while (pixel_list[2*pixel+1] == y + 2) {
-				if ((pixel_list[2*pixel] == x - 2) || (pixel_list[2*pixel] == x - 2)) {
-					colour += 1;
-				} else if ((pixel_list[2*pixel] == x - 1) || (pixel_list[2*pixel] == x - 1)) {
-					colour += 2;
-				} else if (pixel_list[2*pixel] == x) {
-					colour += 4;
-				}
-				pixel++;
-			}
 
-			vga_plot(x, y, (255*colour)/100);			
-		}
-	}
+
+*/
+
+
 	// VGA Blurred dog printing 
 	for (y = 0; y < 120; y++) {
 		for (x =0; x < 160; x++) {
