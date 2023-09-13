304e7c8265129aaa5245c1b871680f5446cb5530
alexm610 <alexmanak@icloud.com>
2023-09-13 15:30:50 -0700

    ubuntu commit initial

--- source/task2/test.s
+++ source/task2/test.s
@@ -1,6 +1,10 @@
 main:
-    movui r2, 0xaa
-    movui r3, 0x0010
-    stwio r2, 0(r3)
+    movia r3, 0x00001010
+    movia r4, 0x08000000
+    movui r2, 0x00
+    stw r2, 0(r3)
 loop:
+    addi r4, r4, 1
+    ldbio r5, 0(r4)
+    stw r5, 0(r3)
     beq zero, zero, loop
